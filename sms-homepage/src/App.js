24fabee531f79f697e8ae1c1f49a58465bc87210
Dawson <59623142+drivers98@users.noreply.github.com>
2020-10-21 17:51:48 -0400

    update 2

--- sms-homepage/src/App.js
+++ sms-homepage/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+ function Body() {
+   return (
+     <p className="App-intro">
+       To get started, edit <code>src/App.js</code> and save to reload.
+     </p>
+   );
+}
+
+
+class Header extends Component {
+  render() {
+    return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">{this.props.title}</h1>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title="Welcome, to SoftScapes SMS" />
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
