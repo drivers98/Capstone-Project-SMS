5d5c9cead57c80ad18f80b776382b22ba0788c09
unknown <tcottre4@kent.edu>
2020-11-19 10:20:13 -0500

    401 login error page

---
+++ sms-homepage/src/pages/401.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import '../App.css';
+
+class NotFound extends Component {
+    render() {
+        return(
+            <h1>401 Unauthorized!</h1>
+        )
+    }
+}
+
+export default NotFound;
